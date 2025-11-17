bfb0d8436cd1599abd6109620370a51951afec3c
Adam Luczak <adam.luczak@softwareone.com>
2025-11-17 13:19:48 +0100

    feat: Update Vite configuration to set base path for repository

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = 'super-duper-octo-enigma.github.io'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react()],
 })
